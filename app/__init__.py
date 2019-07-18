70a4b2396b5b6b3748747d8da85912e1b015d5e7
Christine Yueh <christinecyueh@gmail.com>
2019-07-18 11:50:55 -0400

    Added login

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -15,5 +16,11 @@ bootstrap = Bootstrap(app)
 # app variables for database usage
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# variables for login
+login = LoginManager(app)
+
+# when a page requires someone to login, the application will route them to the correct route
+login.login_view = 'login'
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app, db
 from flask import render_template, url_for, redirect, flash
 from app.forms import TitleForm, ContactForm, LoginForm, RegisterForm, PostForm
-from app.models import Post, Contact
+from app.models import Post, Contact, User
+from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/index')
@@ -52,19 +53,56 @@ def title():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        flash('You are already logged in.')
+        return redirect(url_for('index'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
+        # query the database fo the user trying to log in
+        user = User.query.filter_by(email=form.email.data).first()
+
+        # if user doesn't exist, reload page and flash message
+        if user is None or not user.check_password(form.password.data):
+            flash('Credentials are incorrect.')
+            return redirect(url_for('login'))
+
+        # if user does exist, and credentials are correct, log them in and send them to their profile page
+        login_user(user, remember=form.remember_me.data)
+
         flash('You are now logged in.')
-        return redirect(url_for('profile'))
-        
+        return redirect(url_for('profile', username=current_user.username))
+
     return render_template('form.html', form=form, title='Login')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    # check to see if user is already logged in
+    if current_user.is_authenticated:
+        flash('You are already logged in.')
+        return redirect(url_for('index'))
+
     form = RegisterForm()
 
     if form.validate_on_submit():
+        user = User(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            username = form.username.data,
+            email = form.email.data,
+            url = form.url.data,
+            age = form.age.data,
+            bio = form.bio.data
+        )
+
+        # set password hash
+        user.set_password(form.password.data)
+
+        # add to stage and commit
+        db.session.add(user)
+        db.session.commit()
+
         flash('Thanks for registering!')
         return redirect(url_for('login'))
 
@@ -108,14 +146,16 @@ def contact():
     },\
 #]
 
-@app.route('/profile', methods=['GET', 'POST'])
-def profile():
+@login_required
+@app.route('/profile/<username>', methods=['GET', 'POST'])
+def profile(username=''):
     form = PostForm()
 
     if form.validate_on_submit():
         # step 1: create an instance of the db model
         post = Post(
-            tweet = form.tweet.data
+            tweet = form.tweet.data,
+            user_id = current_user.id
         )
 
         # step 2: add the record to the stage
@@ -124,9 +164,17 @@ def profile():
         # step 3: commit the stage to the db
         db.session.commit()
 
-        return redirect(url_for('profile'))
+        return redirect(url_for('profile', username=username))
 
     # retrieve all posts and pass into view
-    posts = Post.query.all()
+    #posts = Post.query.all()
 
-    return render_template('profile.html', form=form, title='Profile', posts=posts)
+    # pass in user via the username taken in
+    user = User.query.filter_by(username=username).first()
+
+    return render_template('profile.html', form=form, user=user, title='Profile')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
